444a9785ea6e1a4c8f9716b2a81925ac85ee3e38
Adrian Verdugo <adrianverdugo273@gmail.com>
2018-03-14 16:58:31 -0300

    update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# [0.0.2] 14-03-2018
+## Update
+- Ejemplo de [Clase Anidada] JClaseAnidada
+
 # [0.0.1] 13-03-2018
 ## Added
 - Ejemplo de [ENUM] JEnum
--- JEnum/src/jenum/JEnum.java
+++ JEnum/src/jenum/JEnum.java
@@ -1,50 +1,58 @@
-
 package jenum;
+
 import java.util.Scanner;
+
 /**
  *
- * @author Adrian
+ * @author
+ * Catalina Nuñez
+ * Adrian Verdugo
+ * 
  */
 public class JEnum {
 
     public static void main(String[] args) {
-   
+
         Scanner input = new Scanner(System.in);
         System.out.println("Ingresa Valor para verificar si existe");
-        String fruta2 = input.next();
+        
+        // toUpperCase -> para que el valor ingresado este en mayuscula
+        String fruta2 = input.next().toUpperCase();
         Boolean msg = false;
-        
-        /**
-         * Se recorre El enumerador fruit y verfica si existe al ingresar 
-         * el texto a verficar {fruta2}
-         */
-        for(Fruit fruta : Fruit.values()){
-            if(fruta.name().equals(fruta2)) {
-                msg = true;
-            }
-        }
-        
-        /*
-            * Muestra el mensaje dependiendo si existe el objeto 
-            * en el enumerador
-        */
-        if(msg == false) {
-            System.out.printf("%s No existe en el enumerador Fruit \n", fruta2);
-        } 
-        else {
-            System.out.printf("%s Existe en el enumerador Fruit \n", fruta2);
-        }
-        
-        /**
-         * Mostrar los objetos del enumerador Fruit mas sus valores
-         */
-        
-        System.out.println("\n Valores del enumerador");
-        System.out.printf("Nombre \t color \t precio \n");
-        System.out.println("---------------------------------------------------");
-        for(Fruit frutas : Fruit.values()) {
-            System.out.printf("%s \t %s \t %s \n", frutas, frutas.getcolor(), frutas.getPrecio());
+
+        try {
+                /**
+                    * Se recorre El enumerador fruit y verfica si existe al ingresar el
+                    * texto a verficar {fruta2}
+                 */
+                for (Fruit fruta : Fruit.values()) {
+                    if (fruta.name().equals(fruta2)) {
+                        msg = true;
+                    }
+                }
+                /*
+                    * Muestra el mensaje dependiendo si existe el objeto 
+                    * en el enumerador
+                 */
+                if (msg == false) {
+                    System.out.printf("%s No existe en el enumerador Fruit \n", fruta2);
+                } else {
+                    System.out.printf("%s Existe en el enumerador Fruit \n", fruta2);
+                }
+                
+                /**
+                    * Mostrar los objetos del enumerador Fruit mas sus valores
+                */
+                System.out.println("\n Valores del enumerador");
+                System.out.printf("Nombre \t color \t precio \n");
+                System.out.println("---------------------------------------------------");
+                for (Fruit frutas : Fruit.values()) {
+                    System.out.printf("%s \t %s \t %s \n", frutas, frutas.getcolor(), frutas.getPrecio());
+                }
+                
+        } catch (Exception e) {
+            System.out.println(e);
         }
     }
-    
+
 }
